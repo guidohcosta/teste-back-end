b896122d2ded6a72ab18147a5be8dd877efefc2a
guidohcosta <guidohcosta@gmail.com>
2019-05-21 08:35:50 -0300

    add VisitsController

--- front-app/app/controllers/visits_controller.rb
+++ front-app/app/controllers/visits_controller.rb
@@ -1,4 +1,15 @@
 class VisitsController < ApplicationController
   def index
+    @visits = visit_list.visits
+  end
+
+  private
+
+  def visit_list
+    JSON.parse(visit_service.list, object_class: OpenStruct)
+  end
+
+  def visit_service
+    ::VisitService.new
   end
 end
--- front-app/spec/controllers/visits_controller_spec.rb
+++ front-app/spec/controllers/visits_controller_spec.rb
@@ -4,52 +4,20 @@ RSpec.describe VisitsController, type: :controller do
   render_views
   let(:visit_service) { ::VisitService.new }
 
-  describe '#new' do
-    subject { get :new }
-
-    it { is_expected.to have_http_status(:success) }
-    it { is_expected.to render_template(:new) }
-  end
-
-  describe '#create' do
-    subject(:do_create) do
-      post :create,
-        params: {
-          visit: {
-            email: Faker::Name.name,
-            name: Faker::Internet.email
-          }
-        }
-    end
+  describe '#index' do
+    subject(:do_index) { get :index }
 
     before { allow(::VisitService).to receive(:new).and_return(visit_service) }
 
     context 'when successful' do
-      before do
-        allow(visit_service).to receive(:create).and_return('{"status": "success"}')
-      end
-      it { is_expected.to have_http_status(:see_other) } 
-      it { is_expected.to redirect_to page_path('thank-you') } 
+      before { allow(visit_service).to receive(:list).and_return('{"visits": []}') }
+
+      it { is_expected.to have_http_status :ok }
+      it { is_expected.to render_template :index }
       it 'calls service' do
-        expect(visit_service).to receive(:create)
-        do_create
+        expect(visit_service).to receive(:list)
+        do_index
       end
     end
-
-    context 'when unsuccessful' do
-      before do
-        allow(visit_service).to receive(:create).and_return('{"status": "failure"}')
-      end
-      it { is_expected.to have_http_status(:ok) } 
-      it { is_expected.to render_template :new } 
-      it 'calls service' do
-        expect(visit_service).to receive(:create)
-        do_create
-      end
-    end
-  end
-
-  describe '#index' do
-    before { get :index }
   end
 end
